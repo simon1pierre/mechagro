bb0276e0bc732880c163118b8b30d19a95804980
simon1pierre <simonpierreturamyumukiza@gmail.com>
2025-02-12 09:41:27 +0200

    commits

--- app/Http/Controllers/MasterCategoryController.php
+++ app/Http/Controllers/MasterCategoryController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Category;
 class MasterCategoryController extends Controller
 {
     public function storecat(Request $request){
         // validating data quality from form !!!! simon Pierre silence is golden
-        $validate_data = $request()->validate([
+        $validate_data = $request->validate([
             'category_name'=> 'unique:categories|max:100',
         ]);
         
